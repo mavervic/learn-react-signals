5a9f7de9d74b9ca9067c016b54b434e1e7998e90
Vic.Liu <sunset1117@hotmail.com>
2024-01-08 09:46:15 +0800

    feat: 渲染測試

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,71 @@ import { appDogsData } from "./contexts/dogs.context";
 const numDogs = signal(5);
 appDogsData.useFetchBreedsQuery(numDogs);
 
-function App() {
-  function handleClick() {
-    appCount.amountAdded(3);
-    // appCount.increment();
-  }
+function handleClick() {
+  appCount.amountAdded(3);
+  // appCount.increment();
+}
 
+// 就算重複設定也沒關係，因為會自動過濾重複的設定
+// setInterval(() => {
+//   numDogs.value = 5;
+// }, 1000);
+
+const Counter = () => {
+  console.log("Counter render");
+
+  return (
+    <p>
+      <button onClick={handleClick}>count is: {appCount.value} </button>
+    </p>
+  );
+};
+
+const Dog = () => {
+  console.log("Dog render");
+
+  return (
+    <>
+      <div>
+        <p>Dogs to fetch:</p>
+        <select
+          value={numDogs.value}
+          onChange={(e) => (numDogs.value = Number(e.target.value))}
+        >
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+        </select>
+      </div>
+
+      <div>
+        <p>Number of dogs fetched: {appDogsData.value.length}</p>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Picture</th>
+            </tr>
+          </thead>
+          <tbody>
+            {appDogsData.value.map((breed) => (
+              <tr key={breed.id}>
+                <td>{breed.name}</td>
+                <td>
+                  <img src={breed.image.url} alt={breed.name} height={250} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+function App() {
+  console.log("App render");
   // console.log({
   //   appCount,
   //   numDogs,
@@ -24,44 +83,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={handleClick}>count is: {appCount.value} </button>
-        </p>
+        <Counter />
 
-        <div>
-          <p>Dogs to fetch:</p>
-          <select
-            value={numDogs.value}
-            onChange={(e) => (numDogs.value = Number(e.target.value))}
-          >
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="15">15</option>
-            <option value="20">20</option>
-          </select>
-        </div>
-
-        <div>
-          <p>Number of dogs fetched: {appDogsData.value.length}</p>
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Picture</th>
-              </tr>
-            </thead>
-            <tbody>
-              {appDogsData.value.map((breed) => (
-                <tr key={breed.id}>
-                  <td>{breed.name}</td>
-                  <td>
-                    <img src={breed.image.url} alt={breed.name} height={250} />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        <Dog />
 
         <p>
           <a
